48d122d9df20987d5859dc323c271598425fb01c
Totsylol <thefishjonah@gmail.com>
2025-06-23 00:17:21 -0400

    dwdw

--- AppArch/frontend/Wedding/src/App.js
+++ AppArch/frontend/Wedding/src/App.js
@@ -1,39 +1,35 @@
+// src/App.js
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Homepage from "./components/Homepage";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Gallery from "./components/Gallery";
-import WeddingParty from "./components/WeddingParty";
-import OurStory from './components/OurStory';
-import MoreInfo from './components/MoreInfo';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
 
+import Homepage from './components/Homepage';
+import Navbar   from './components/Navbar';
+import Footer   from './components/Footer';
+import Gallery      from './components/Gallery';
+import WeddingParty from './components/WeddingParty';
+import OurStory     from './components/OurStory';
+import MoreInfo     from './components/MoreInfo';
 
 function App() {
-  const [lightboxOpen, setLightboxOpen] = useState(false); // Control Navbar visibility
+  const [lightboxOpen, setLightboxOpen] = useState(false);
 
   return (
     <div className="App">
       <Router>
-        {/* Conditionally render Navbar */}
-        {!lightboxOpen && <Navbar />}
-        <Switch>
-          <Route exact path="/">
-            <Homepage />
-          </Route>
-          <Route path="/gallery">
-            <Gallery setLightboxOpen={setLightboxOpen} /> {/* Pass setter to Gallery */}
-          </Route>
-          <Route path="/wedding_party">
-        <WeddingParty />
-        </Route>
-        <Route path="/Our Story">
-      <OurStory/>
-      </Route>
-      <Route path="/info">
-  <MoreInfo />
-</Route>
-        </Switch>
+        { !lightboxOpen && <Navbar /> }
+
+        <Routes>
+          <Route path="/"            element={<Homepage />} />
+          <Route path="/gallery"     element={<Gallery setLightboxOpen={setLightboxOpen} />} />
+          <Route path="/wedding_party" element={<WeddingParty />} />
+          <Route path="/Our Story"    element={<OurStory />} />
+          <Route path="/info"         element={<MoreInfo />} />
+        </Routes>
+
         <Footer />
       </Router>
     </div>
